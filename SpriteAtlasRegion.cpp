4fb790e3222833c89233d6232a482545df5c9c13
AstralCode <gorski.bm@gmail.com>
2021-02-01 12:20:19 +0100

    Refactoring sprite atlas region class

--- SpriteAtlasRegion.cpp
+++ SpriteAtlasRegion.cpp
@@ -1,33 +1,41 @@
 #include "SpriteAtlasRegion.hpp"
 
-SpriteAtlasRegion::SpriteAtlasRegion(const sf::Vector2i& offset, const sf::Vector2i& spriteSize, const std::vector<std::vector<sf::IntRect>>& areas)
+SpriteAtlasRegion::SpriteAtlasRegion(const sf::Vector2i& offset, const sf::Vector2i& spriteSize, const std::vector<std::vector<sf::IntRect>>& spriteAreas)
 {
-    initializeSpriteAreas(offset, spriteSize, areas);
+    calculateSpriteAreas(offset, spriteSize, spriteAreas);
 }
 
 const sf::IntRect& SpriteAtlasRegion::getSprite(const int index) const
 {
-    return mSpriteAreas[index];
+    return mSprites[index];
 }
 
 int SpriteAtlasRegion::getSpriteCount() const
 {
-    return static_cast<int>(mSpriteAreas.size());
+    return static_cast<int>(mSprites.size());
 }
 
-void SpriteAtlasRegion::initializeSpriteAreas(const sf::Vector2i& offset, const sf::Vector2i& spriteSize, const std::vector<std::vector<sf::IntRect>>& areas)
+void SpriteAtlasRegion::calculateSpriteAreas(const sf::Vector2i& offset, const sf::Vector2i& spriteSize, const std::vector<std::vector<sf::IntRect>>& spriteAreas)
 {
-    for (int j{0}; j < static_cast<int>(areas.size()); j++)
+    for (int j{0}; j < static_cast<int>(spriteAreas.size()); j++)
     {
-        for (int i{0}; i < static_cast<int>(areas[j].size()); i++)
+        for (int i{0}; i < static_cast<int>(spriteAreas[j].size()); i++)
         {
-            sf::IntRect spriteArea{};
-            spriteArea.left = (offset.x * spriteSize.x) + (i * spriteSize.x) + areas[j][i].left;
-            spriteArea.top = (offset.y * spriteSize.y) + (j * spriteSize.y) + areas[j][i].top;
-            spriteArea.width = areas[j][i].width;
-            spriteArea.height = areas[j][i].height;
+            sf::Vector2i regionOffset{};
+            regionOffset.x = offset.x * spriteSize.x;
+            regionOffset.y = offset.y * spriteSize.y;
 
-            mSpriteAreas.push_back(spriteArea);
+            sf::Vector2i spriteAreaOffset{};
+            spriteAreaOffset.x = i * spriteSize.x;
+            spriteAreaOffset.y = j * spriteSize.y;
+
+            sf::IntRect sprite{};
+            sprite.left = regionOffset.x + spriteAreaOffset.x + spriteAreas[j][i].left;
+            sprite.top = regionOffset.y + spriteAreaOffset.y + spriteAreas[j][i].top;
+            sprite.width = spriteAreas[j][i].width;
+            sprite.height = spriteAreas[j][i].height;
+
+            mSprites.push_back(sprite);
         }
     }
 }
